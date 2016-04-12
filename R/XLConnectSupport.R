3ca5592414839e234fb253240a53e8fd126991a3
SCOTT FURLAN <scottfurlan@gmail.com>
2016-04-12 10:55:08 -0700

    10:55a

--- R/XLConnectSupport.R
+++
@@ -1,5 +0,0 @@
-##XLConnect SUPPORT###
-filename<-(paste(rootpath,"/Bioinformatics Resources/Templates/template.xls", sep=""))
-style.wb <- loadWorkbook (filename)
-cellstyle <- getCellStyle (style.wb, "GeneData")
-rm(style.wb)
