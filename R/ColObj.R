064f76b2ea41d3284811131f10fed5c20ca91683
SCOTT FURLAN <scottfurlan@gmail.com>
2016-07-28 10:59:30 -0700

    Added ColObj

---
+++ R/ColObj.R
@@ -0,0 +1,30 @@
+##ColObj##
+#This set of functions allows for easy color reference for microarray analysis.
+#Essentially, the ColObj has three slots 1) an assignment slot 2) a slot for full color
+#reference (i.e for every sample) and 3) a match slot for an ordered list of colors by matching classvec
+
+setClass("ColObj", slots=c(
+  assign="data.frame",
+  classvec="factor",
+  full="list",
+  match="list"), package="probedeeper")
+
+
+
+ColObjInit<-function(ColObj, LD=80){
+  if(class(ColObj)!="ColObj"){stop("Input is not a ColObj class")}
+  selected<-levels(classvec)
+  colmatch<-paste("#", ColObj@assign$Color[match(selected,ColObj@assign$Group)], sep="")
+  names(colmatch)<-ColObj@assign$Group[match(selected,ColObj@assign$Group)]
+  ColObj@full$line<-colmatch[match(classvec, names(colmatch))]
+  ColObj@full$fill<-sapply(ColObj@full$line, LightenDarkenColor, LD)
+  ColObj@match$line<-colmatch
+  ColObj@match$fill<-sapply(colmatch, LightenDarkenColor, LD)
+  par(mfrow=c(1,2))
+  pie(rep(1,length(ColObj@match$line)), col=ColObj@match$line, labels=names(ColObj@match$line))
+  pie(rep(1,length(ColObj@match$fill)), col=ColObj@match$fill, labels=names(ColObj@match$fill))
+  return(ColObj)
+}
+
+
+
