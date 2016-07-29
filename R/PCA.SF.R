38619b13668c223c9e182fdecdf3f237f8faecd9
SCOTT FURLAN <scottfurlan@gmail.com>
2016-07-28 21:50:26 -0700

    bug

--- R/PCA.SF.R
+++ R/PCA.SF.R
@@ -17,9 +17,9 @@ for(g in levels(dfr$group)){
   dfr_ell <- rbind(dfr_ell, cbind(data.frame(with(tempdfr,ellipse::ellipse(as.numeric(cor(x, y)), scale=c(sd(x),sd(y)),
                                                                centre=c(mean(x),mean(y)), level=level))),group=g))
 }
-g<-ggplot2::ggplot(data=dfr, aes(x=x, y=y)) + ggplot2::geom_point(colour=patientcolors, size=1.5, alpha=1)+
-  ggplot2::geom_point(data=centroids, aes(x=x, y=y), colour=colors, size=3, shape=43) +
-  ggplot2::geom_segment(aes(x=x.centroid, y=y.centroid, xend=x, yend=y), colour=patientcolors, size=0.2)+
+g<-ggplot2::ggplot(data=dfr, ggplot2::aes(x=x, y=y)) + ggplot2::geom_point(colour=patientcolors, size=1.5, alpha=1)+
+  ggplot2::geom_point(data=centroids, ggplot2::aes(x=x, y=y), colour=colors, size=3, shape=43) +
+  ggplot2::geom_segment(ggplot2::aes(x=x.centroid, y=y.centroid, xend=x, yend=y), colour=patientcolors, size=0.2)+
   ggplot2::geom_hline(yintercept = 0)+
   ggplot2::geom_vline(xintercept = 0)+
   ggplot2::theme_bw()+
@@ -27,8 +27,8 @@ g<-ggplot2::ggplot(data=dfr, aes(x=x, y=y)) + ggplot2::geom_point(colour=patient
   ggplot2::theme(axis.ticks.y = element_blank(), axis.text.y = element_blank())+
   ggplot2::labs(x=deparse(substitue(xdata)), y=deparse(substitue(ydata)), title=title)
 for(i in 1:length(levels(as.factor(dfr_ell$group)))){
-  g<-g+ggplot2::geom_path(data=dfr_ell[dfr_ell$group==levels(as.factor(dfr_ell$group))[i],], aes(x=x, y=y), size=0.2, colour=levels(factor(patientcolors, levels = unique(patientcolors)))[i], linetype=1)
-  g<-g+ggplot2::geom_polygon(data=dfr_ell[dfr_ell$group==levels(as.factor(dfr_ell$group))[i],], aes(x=x, y=y), fill=levels(factor(patientcolors, levels = unique(patientcolors)))[i], alpha=0.1, linetype=0) }
+  g<-g+ggplot2::geom_path(data=dfr_ell[dfr_ell$group==levels(as.factor(dfr_ell$group))[i],], ggplot2::aes(x=x, y=y), size=0.2, colour=levels(factor(patientcolors, levels = unique(patientcolors)))[i], linetype=1)
+  g<-g+ggplot2::geom_polygon(data=dfr_ell[dfr_ell$group==levels(as.factor(dfr_ell$group))[i],], ggplot2::aes(x=x, y=y), fill=levels(factor(patientcolors, levels = unique(patientcolors)))[i], alpha=0.1, linetype=0) }
 
 print(g)
 return(g)
