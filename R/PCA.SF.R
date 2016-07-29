d2d7ba075376ef74bfa6ee390236399cdd98e6f3
SCOTT FURLAN <scottfurlan@gmail.com>
2016-07-28 21:48:21 -0700

    fixed function calls in PCA.SF added better axis labels

--- R/PCA.SF.R
+++ R/PCA.SF.R
@@ -14,21 +14,21 @@ centroids <- aggregate(cbind(x,y)~group,data=dfr,mean)
 dfr <- merge(dfr,centroids,by="group",suffixes=c("",".centroid"))
 for(g in levels(dfr$group)){
   tempdfr<-dfr[dfr$group==g,]
-  dfr_ell <- rbind(dfr_ell, cbind(data.frame(with(tempdfr,ellipse(as.numeric(cor(x, y)), scale=c(sd(x),sd(y)),
+  dfr_ell <- rbind(dfr_ell, cbind(data.frame(with(tempdfr,ellipse::ellipse(as.numeric(cor(x, y)), scale=c(sd(x),sd(y)),
                                                                centre=c(mean(x),mean(y)), level=level))),group=g))
 }
-g<-ggplot(data=dfr, aes(x=x, y=y)) + geom_point(colour=patientcolors, size=1.5, alpha=1)+
-  geom_point(data=centroids, aes(x=x, y=y), colour=colors, size=3, shape=43) +
-  geom_segment(aes(x=x.centroid, y=y.centroid, xend=x, yend=y), colour=patientcolors, size=0.2)+
-  geom_hline(yintercept = 0)+
-  geom_vline(xintercept = 0)+
-  theme_bw()+
-  theme(axis.ticks.x = element_blank(), axis.text.x = element_blank())+
-  theme(axis.ticks.y = element_blank(), axis.text.y = element_blank())+
-  labs(x="PC1", y="PC2", title=title)
+g<-ggplot2::ggplot(data=dfr, aes(x=x, y=y)) + ggplot2::geom_point(colour=patientcolors, size=1.5, alpha=1)+
+  ggplot2::geom_point(data=centroids, aes(x=x, y=y), colour=colors, size=3, shape=43) +
+  ggplot2::geom_segment(aes(x=x.centroid, y=y.centroid, xend=x, yend=y), colour=patientcolors, size=0.2)+
+  ggplot2::geom_hline(yintercept = 0)+
+  ggplot2::geom_vline(xintercept = 0)+
+  ggplot2::theme_bw()+
+  ggplot2::theme(axis.ticks.x = element_blank(), axis.text.x = element_blank())+
+  ggplot2::theme(axis.ticks.y = element_blank(), axis.text.y = element_blank())+
+  ggplot2::labs(x=deparse(substitue(xdata)), y=deparse(substitue(ydata)), title=title)
 for(i in 1:length(levels(as.factor(dfr_ell$group)))){
-  g<-g+geom_path(data=dfr_ell[dfr_ell$group==levels(as.factor(dfr_ell$group))[i],], aes(x=x, y=y), size=0.2, colour=levels(factor(patientcolors, levels = unique(patientcolors)))[i], linetype=1)
-  g<-g+geom_polygon(data=dfr_ell[dfr_ell$group==levels(as.factor(dfr_ell$group))[i],], aes(x=x, y=y), fill=levels(factor(patientcolors, levels = unique(patientcolors)))[i], alpha=0.1, linetype=0) }
+  g<-g+ggplot2::geom_path(data=dfr_ell[dfr_ell$group==levels(as.factor(dfr_ell$group))[i],], aes(x=x, y=y), size=0.2, colour=levels(factor(patientcolors, levels = unique(patientcolors)))[i], linetype=1)
+  g<-g+ggplot2::geom_polygon(data=dfr_ell[dfr_ell$group==levels(as.factor(dfr_ell$group))[i],], aes(x=x, y=y), fill=levels(factor(patientcolors, levels = unique(patientcolors)))[i], alpha=0.1, linetype=0) }
 
 print(g)
 return(g)
